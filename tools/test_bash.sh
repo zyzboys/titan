7f2bc958d81b2473e3080e6f7e609bb90d44c7b3
zhangyuzhan <511586683@qq.com>
2024-05-21 22:04:11 +0800

    fix

--- tools/test_bash.sh
+++ tools/test_bash.sh
@@ -3,16 +3,8 @@ thread_count=8
 echo "start exp1"
 for val_size in 256 512 1024 2048 4096
 do
-    entry_count=$(((target_size_bytes / (val_size + 16)) / thread_count) * thread_count)
+    entry_count=$((target_size_bytes / (val_size + 16) / thread_count * thread_count))
     ycsb_entry_count=$((entry_count * 2))
     echo "entry count: $entry_count"
     echo "ycsb entry count: $ycsb_entry_count"
-
 done
-
-#for skewness in 0 0.3 0.6 0.9
-# do  
-#     entry_count=$((target_size_bytes / (1024 + 16)))
-#     echo "entry count: $entry_count"
-
-# done
